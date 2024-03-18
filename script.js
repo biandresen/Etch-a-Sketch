67343ba4b68f53ca7d8cc7e5d0edc9ad93187d7b
birando <birger1@live.com>
2024-03-19 00:33:08 +0100

    feat: add shading mode

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const divContainer = document.getElementById("container");
 const newDrawingBtn = document.getElementById("newDrawing");
 const modeNormalBtn = document.getElementById("modeNormal");
 const modeColorBtn = document.getElementById("modeColor");
-//const modeShadeBtn = document.getElementById("modeShade");
+const modeShadeBtn = document.getElementById("modeShade");
+let shades = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 let mode = 0;
 let r = 0;
 let g = 0;
@@ -17,6 +18,11 @@ modeNormalBtn.addEventListener("click", () => {
 });
 modeColorBtn.addEventListener("click", () => {
   mode = 1;
+  selectSquares();
+});
+modeShadeBtn.addEventListener("click", () => {
+  mode = 2;
+  shade = 0;
   selectSquares();
 });
 
@@ -46,15 +52,41 @@ function addNewDrawing(num) {
       div.style.width = "calc(100%/" + num + ")";
       div.style.height = "calc(100%/" + num + ")";
       divContainer.appendChild(div);
-      //Add new color to div squares
+      //Add black color to div squares
       if (mode === 0) {
         div.addEventListener("mouseenter", () => {
           div.classList.add("colorChanged");
         });
+        //Add random color to div squares
       } else if (mode === 1) {
         div.addEventListener("mouseenter", () => {
           addRandomColor();
           div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+        });
+        //Add shading to div squares
+      } else if (mode === 2) {
+        div.addEventListener("mouseenter", () => {
+          if (div.style.backgroundColor === "") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[0] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.1)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[1] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.2)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[2] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.3)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[3] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.4)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[4] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.5)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[5] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.6)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[6] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.7)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[7] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.8)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[8] + ")";
+          } else if (div.style.backgroundColor === "rgba(0, 0, 0, 0.9)") {
+            div.style.backgroundColor = "rgba(0, 0, 0," + shades[9] + ")";
+          }
         });
       }
     }
